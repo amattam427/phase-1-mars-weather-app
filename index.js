36f2b5158ae73080c89ba5e4c41225fedc62495d
Eleni <elenipapanicolas@gmail.com>
2021-12-06 16:52:08 -0500

    added weather object for recs

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const weatherEndpoint = 'https://api.maas2.apollorion.com/';
 
-// sol here needs to correspond to sol fetched by weather API
 let sol = getRandomSol();
 const pictureAPIKey = 'cO4ZKLydH6VHd21eDz3InRVoJISzRK6UQBTflnD9';
-const filterBySolEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${pictureAPIKey}`;
+const filterBySolEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}camera=PANCAM&api_key=${pictureAPIKey}`;
 
 // html elements to grab
 const solSpan = document.getElementById('sol-number');
@@ -24,7 +23,9 @@ const viewMoreButton = document.getElementById('view-more');
 const toggleButton = document.getElementById('toggle-temp');
 const infoModal = document.getElementById('xt-info-modal');
 const degrees = document.querySelectorAll('#c-or-f');
+const recsModal = document.getElementById('recs-modal');
 
+// event listeners
 document.addEventListener('DOMContentLoaded', initPictures);
 document.addEventListener('DOMContentLoaded', initWeather);
 viewMoreButton.addEventListener('click', () => {
@@ -35,7 +36,7 @@ viewMoreButton.addEventListener('click', () => {
        } else if (moreOrLess.innerText === 'less') {
            moreOrLess.innerText = 'more';
        }
-})
+});
 
 // add event listener for click on clothing recs
 // add event listener for toggle temp
@@ -49,21 +50,19 @@ function initPictures () {
     const pictureData = fetch(filterBySolEndpoint)
     .then(res => res.json())
     .then(data => {
-        console.log(data.photos.length)
+        // console.log(data.photos)
         const howManyPhotos = data.photos.length;
         function getRandomNumberPic () {
             const num = Math.ceil(Math.random() * howManyPhotos);
             return num;
         }
+
         const photoToShow1 = data.photos[getRandomNumberPic()];
         const photoToShow2 = data.photos[getRandomNumberPic()];
         const photoToShow3 = data.photos[getRandomNumberPic()];
         const image1 = document.createElement('img');
         const image2 = document.createElement('img');
         const image3 = document.createElement('img');
-        // const caption1 = document.createElement('caption');
-        // const caption2 = document.createElement('caption');
-        // const caption3 = document.createElement('caption');
         image1.src = photoToShow1.img_src;
         image2.src = photoToShow2.img_src;
         image3.src = photoToShow3.img_src;
@@ -94,8 +93,21 @@ function initWeather () {
         humiditySpan.innerText = solObject.abs_humidity || 'not currently available';
         windSpan.innerText = solObject.wind_speed || 'not currently available';
         
+
+        function clothingRecs() {
+            if (solObject.atmo_opacity === 'Sunny' || 'sunny') {
+                const recs = weatherObj.sunny.recommendations;
+                console.log(recs);
+
+            }
+       }
+       clothingRecs();
     })
+
+   
 }
+
+// need to format display time for sunrise and sunset
 
 // replace colors with proper backgrounds
 // possibly do it as add classlist and make css classes
@@ -106,36 +118,47 @@ function timeOfDay () {
     const currentHour = now.getHours();
     const body = document.getElementById('body');
     if (currentHour < 6 || currentHour > 19) {
-        body.style.backgroundColor = 'navy';
+        body.style.backgroundColor = '#191970';
     } else if (currentHour > 6 && currentHour < 8) {
-        body.style.backgroundColor = 'orange';
+        body.style.backgroundColor = '#FFA07A';
     } else if (currentHour > 8 && currentHour < 17) {
-        body.style.backgroundColor = 'yellow';
+        body.style.backgroundColor = '#FAFAD2';
     } else if (currentHour > 17 && currentHour < 19) {
-        body.style.backgroundColor = 'blue';
+        body.style.backgroundColor = '#87CEFA';
     }
 }
 timeOfDay();
 
-// const weatherObj = {
-//     {
-//         conditions: 'sunny',
-//         recommendations: {
-//             1: `Don't forget sunscreen!`,
-//             2: 
-//         }
-//     },
-//     {
-
-//     }
-// }
-
-function clothingRecs() {
-
+const weatherObj = {
+        sunny: {
+            recommendations: {
+                1: `Don't forget your sunscreen!`,
+                2: `Wear a hat for protection!`,
+                3: `Drink lots of water today!`
+            }
+        },
+        cloudy: {
+            recommendations: {
+                1: `Bring an umbrella in case it rains!`,
+                2: `Dont forget you can still get a sunburn through the clouds!`
+            }
+        },
+        rainy: {
+            recommendations: {
+                1: `Bring an umbrella!`,
+                2: `Wear your rainboots!`
+            }
+        }
 }
 
 
+
 // define a function that converts celsius to farenheit
+
+function convertCToF (degreesInC) {
+   
+
+}
 
 // define a function that pops up a modal of information when clothing recommendation is clicked
 // define an object with all possible weather conditions outcomes and 
