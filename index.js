03d78907ab018c721e32a934964c9ff044cc1008
Eleni <elenipapanicolas@gmail.com>
2021-12-06 11:12:05 -0500

    added function to get time of day and change background accordingly

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
- const weatherEndpoint = 'https://api.maas2.apollorion.com/';
+const weatherEndpoint = 'https://api.maas2.apollorion.com/';
 
 // sol here needs to correspond to sol fetched by weather API
 let sol = 3317; 
 let page = 1;
- const pictureAPIKey = 'cO4ZKLydH6VHd21eDz3InRVoJISzRK6UQBTflnD9';
- const filterBySolEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&page=${page}&api_key=${pictureAPIKey}`;
-
-const todaysDate = new Date();
-const formattedDate = todaysDate.toDateString();
+const pictureAPIKey = 'cO4ZKLydH6VHd21eDz3InRVoJISzRK6UQBTflnD9';
+const filterBySolEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&page=${page}&api_key=${pictureAPIKey}`;
 
 // html elements to grab
 const solSpan = document.getElementById('sol-number');
@@ -27,6 +24,7 @@ const clothingButton = document.getElementById('clothing-recs');
 const viewMoreButton = document.getElementById('view-more');
 const toggleButton = document.getElementById('toggle-temp');
 const infoModal = document.getElementById('xt-info-modal');
+const degrees = document.querySelectorAll('#c-or-f');
 
 document.addEventListener('DOMContentLoaded', initPictures);
 document.addEventListener('DOMContentLoaded', initWeather);
@@ -55,29 +53,69 @@ function initPictures () {
     return pictureData;
 }
 
+
 function initWeather () {
     fetch(weatherEndpoint)
     .then(res => res.json())
     .then(solObject => {
         // here we will populate weather info
+        const terrestrial = solObject.terrestrial_date;
+        const earthDate = new Date(terrestrial);
+
         solSpan.innerText = solObject.sol;
         seasonSpan.innerText = solObject.season;
-        earthDateSpan.innerText = solObject.terrestrial_date;
+        earthDateSpan.innerText = earthDate.toDateString();
         conditionsSpan.innerText = solObject.atmo_opacity;
         hiTempSpan.innerText = solObject.max_temp;
         lowTempSpan.innerText = solObject.min_temp;
         sunriseSpan.innerText = solObject.sunrise;
         sunsetSpan.innerText = solObject.sunset;
         uvSpan.innerText = solObject.local_uv_irradiance_index;
-        pressureSpan.innerText = solObject.pressure;
+        pressureSpan.innerText = solObject.pressure_string + ' ' + solObject.pressure;
         humiditySpan.innerText = solObject.abs_humidity || 'not currently available';
         windSpan.innerText = solObject.wind_speed || 'not currently available';
         
     })
 }
 
+// replace colors with proper backgrounds
+// possibly do it as add classlist and make css classes
+// that match each time of day
 
-// format date function - can get from lecture
+function timeOfDay () {
+    const now = new Date ();
+    const currentHour = now.getHours();
+    const body = document.getElementById('body');
+    if (currentHour < 6 || currentHour > 19) {
+        body.style.backgroundColor = 'navy';
+    } else if (currentHour > 6 && currentHour < 8) {
+        body.style.backgroundColor = 'orange';
+    } else if (currentHour > 8 && currentHour < 17) {
+        body.style.backgroundColor = 'yellow';
+    } else if (currentHour > 17 && currentHour < 19) {
+        body.style.backgroundColor = 'blue';
+    }
+}
+timeOfDay();
+
+// const weatherObj = {
+//     {
+//         conditions: 'sunny',
+//         recommendations: {
+//             1: `Don't forget sunscreen!`,
+//             2: 
+//         }
+//     },
+//     {
+
+//     }
+// }
+
+function clothingRecs() {
+
+}
+
+// format date function
 
 // define a function to return a number that is the sol to query for pictures (max number is 3000)
 
@@ -86,15 +124,8 @@ function initWeather () {
 
 // define a function that converts celsius to farenheit
 
-// define a function that toggles the farenheit/celsius when clicked
-
-// define a function that toggles hide/view for extra information
-
 // define a function that pops up a modal of information when clothing recommendation is clicked
 // define an object with all possible weather conditions outcomes and 
 // corresponding recommendations
-
-// define a function that changes the background or color style
-// based on time of day in relation to before sunrise, during the day, and after sunset
 
 // define a function that can fetch any previous sol and then populate app with data
