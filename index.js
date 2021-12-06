1c3c973cbb4d8d5bb4dfd30cb0e5e7518544b2c5
Eleni <elenipapanicolas@gmail.com>
2021-12-06 10:28:22 -0500

    selected html elements and populated with api info

--- index.js
+++ index.js
@@ -7,17 +7,50 @@ let page = 1;
  const filterBySolEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&page=${page}&api_key=${pictureAPIKey}`;
 
 const todaysDate = new Date();
+const formattedDate = todaysDate.toDateString();
+
+// html elements to grab
+const solSpan = document.getElementById('sol-number');
+const seasonSpan = document.getElementById('szn-number');
+const earthDateSpan = document.getElementById('earth-date');
+const imageContainer = document.getElementById('image-container');
+const conditionsSpan = document.getElementById('conditions');
+const hiTempSpan = document.getElementById('hi-temp');
+const lowTempSpan = document.getElementById('low-temp');
+const sunriseSpan = document.getElementById('sunrise');
+const sunsetSpan = document.getElementById('sunset');
+const uvSpan = document.getElementById('uv');
+const pressureSpan = document.getElementById('pressure');
+const humiditySpan = document.getElementById('humidity');
+const windSpan = document.getElementById('wind');
+const clothingButton = document.getElementById('clothing-recs');
+const viewMoreButton = document.getElementById('view-more');
+const toggleButton = document.getElementById('toggle-temp');
+const infoModal = document.getElementById('xt-info-modal');
 
 document.addEventListener('DOMContentLoaded', initPictures);
 document.addEventListener('DOMContentLoaded', initWeather);
+viewMoreButton.addEventListener('click', (e) => {
+    const moreOrLess = document.getElementById('more-or-less');
+    infoModal.classList.toggle('hidden');
+       if (moreOrLess.innerText === 'more') {
+           moreOrLess.innerText = 'less';
+       } else if (moreOrLess.innerText === 'less') {
+           moreOrLess.innerText = 'more';
+       }
+})
+
+// add event listener for click on clothing recs
+// add event listener for toggle temp
+
 
 function initPictures () {
     const pictureData = fetch(filterBySolEndpoint)
     .then(res => res.json())
     .then(data => {
-        data.photos.forEach(picture => {
-            console.log(picture)
-        })
+        // data.photos.forEach(picture => {
+        //     console.log(picture)
+        // })
     })
     return pictureData;
 }
@@ -26,23 +59,25 @@ function initWeather () {
     fetch(weatherEndpoint)
     .then(res => res.json())
     .then(solObject => {
-        const todaysSol = solObject.sol; 
-        const weatherConditions = solObject.atmo_opacity;
-        const uvIrradiance = solObject.local_uv_irradiance_index;
-        const hiTemp = solObject.max_temp;
-        const lowTemp = solObject.min_temp;
-        const pressure = solObject.pressure;
-        const season = solObject.season;
-        const sunrise = solObject.sunrise;
-        const sunset = solObject.sunset;
-        const dateOnAEarth = solObject.terrestrial_date;
-        console.log(solObject)
-        // here we will need to grab html element containers
-        // then create new html elements for this info and populate
+        // here we will populate weather info
+        solSpan.innerText = solObject.sol;
+        seasonSpan.innerText = solObject.season;
+        earthDateSpan.innerText = solObject.terrestrial_date;
+        conditionsSpan.innerText = solObject.atmo_opacity;
+        hiTempSpan.innerText = solObject.max_temp;
+        lowTempSpan.innerText = solObject.min_temp;
+        sunriseSpan.innerText = solObject.sunrise;
+        sunsetSpan.innerText = solObject.sunset;
+        uvSpan.innerText = solObject.local_uv_irradiance_index;
+        pressureSpan.innerText = solObject.pressure;
+        humiditySpan.innerText = solObject.abs_humidity || 'not currently available';
+        windSpan.innerText = solObject.wind_speed || 'not currently available';
         
     })
 }
 
+
+// format date function - can get from lecture
 
 // define a function to return a number that is the sol to query for pictures (max number is 3000)
 
